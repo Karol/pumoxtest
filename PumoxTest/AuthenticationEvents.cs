0f7aeb14b1692f38bb29890f480a91e259ccc044
Karol korol <karolkorol@gmail.com>
2018-10-06 10:43:34 +0200

    add basicAuthentication

---
+++ PumoxTest/AuthenticationEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ZNetCS.AspNetCore.Authentication.Basic;
+using ZNetCS.AspNetCore.Authentication.Basic.Events;
+
+namespace PumoxTest
+{
+    public class AuthenticationEvents : BasicAuthenticationEvents
+    {
+        public override Task ValidatePrincipalAsync(ValidatePrincipalContext context)
+        {
+            if ((context.UserName == "test") && (context.Password == "test"))
+            {
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, context.UserName, context.Options.ClaimsIssuer)
+            };
+
+                var principal = new ClaimsPrincipal(new ClaimsIdentity(claims, BasicAuthenticationDefaults.AuthenticationScheme));
+                context.Principal = principal;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- PumoxTest/Startup.cs
+++ PumoxTest/Startup.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PumoxTest.DataBase;
+using PumoxTest.DataBase.Repositories;
+using ZNetCS.AspNetCore.Authentication.Basic;
 
 namespace PumoxTest
 {
@@ -25,12 +32,44 @@ namespace PumoxTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<PumoxTestContext>(o => o.UseSqlServer(connectionString));
+
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartHeadersLengthLimit = int.MaxValue;
+                x.BufferBodyLengthLimit = long.MaxValue;
+                x.MemoryBufferThreshold = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue; // In case of multipart
+            });
+
+            services.AddScoped<AuthenticationEvents>();
+
+            services
+                .AddAuthentication(BasicAuthenticationDefaults.AuthenticationScheme)
+                .AddBasicAuthentication(
+                    options =>
+                    {
+                        options.Realm = "PumoxTest";
+                        options.EventsType = typeof(AuthenticationEvents);    
+                    });
+
+            // configure DI for application services
+            //services.AddScoped<IUserRepository, UserService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,9 +78,15 @@ namespace PumoxTest
             {
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
+
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<PumoxTestContext>();
+                context.Database.Migrate();
+                DbInitializer.Initialize(context);
+            }
         }
     }
 }
