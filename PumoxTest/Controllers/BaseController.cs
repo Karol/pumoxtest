145da55bb489621247d00f40ee2603dd5441df4a
Karol korol <karolkorol@gmail.com>
2018-10-06 10:39:43 +0200

    add BaseController

---
+++ PumoxTest/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PumoxTest.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PumoxTest.Controllers
+{
+    public class BaseController : Controller
+    {
+        private UnitOfWork _unitOfWork;
+        protected IConfiguration Configuration { get; set; }
+
+        public BaseController(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        /// <summary>
+        /// UnitOfWork
+        /// </summary>
+        protected UnitOfWork UnitOfWork
+        {
+            get
+            {
+                if (_unitOfWork == null)
+                {
+                    var conString = Configuration.GetConnectionString("DefaultConnection");
+                    _unitOfWork = new UnitOfWork(conString);
+                }
+                return _unitOfWork;
+            }
+        }
+    }
+}
